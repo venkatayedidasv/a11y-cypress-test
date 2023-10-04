717137f00987a49712ede743d6c6da941e3e99f2
Venkata Yedida <venkata.yedida@ingka.ikea.com>
2023-10-04 21:42:36 +0200

    Inital commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("cypress");
 const fs = require('fs');
+const path = require('path');
 
 module.exports = defineConfig({
   // Your other Cypress configuration options here
@@ -12,16 +13,17 @@ module.exports = defineConfig({
       on('task', {
         log(message) {
           console.log(message);
-
           return null;
         },
         table(message) {
           console.table(message);
-          fs.appendFile('log.json', JSON.stringify(message, null, 2) + '\n', (err) => {
-                      if (err) {
-                        console.error('Error writing to log.json:', err);
-                      }
-                      });
+          // Determine the path to the log.json file
+          const logFilePath = path.join(__dirname, 'log.json');
+          fs.appendFile(logFilePath, JSON.stringify(message, null, 2) + '\n', (err) => {
+          if (err) {
+             console.error('Error writing to log.json:', err);
+          }
+          });
           return null;
         },
       });
