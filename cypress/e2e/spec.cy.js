3031d0bee22dcf2788ecb447eda7eed28d3c39f2
Venkata Yedida <venkata.yedida@ingka.ikea.com>
2023-10-10 22:37:52 +0200

    added log

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -23,7 +23,7 @@ function terminalLog(violations) {
 
 describe('template spec', () => {
   it('passes', () => {
-    const testUrl = Cypress.env('CYPRESS_TEST_URL');
+    const testUrl = Cypress.env('test_url');
     cy.log(`test_url: ${testUrl}`);
     cy.visit(testUrl)
     cy.injectAxe()
